6dcd63a0ba5c0a37d3ebc483a6698614b959a7f8
Erik Cunningham <ewc@absolute-performance.com>
2011-08-22 17:39:05 -0600

    move zsh to files

--- zsh/func/campfire.sh
+++
@@ -1,5 +0,0 @@
-
-function opschat {
- message="{\"message\":{\"body\":\"$@\"}}"
- curl -v -H'Content-Type: application/json' -d "$message" -u 48742007985bf1dc7b2918eaa07bcba1f878baff:X https://absperf.campfirenow.com/room/277790/speak.json
-}
