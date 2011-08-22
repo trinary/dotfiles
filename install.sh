692450e1a8f704baa0476bfd6cbd6edb333ee698
Erik Cunningham <ewc@absolute-performance.com>
2011-08-22 17:56:12 -0600

    installer that does something

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-for i in ./files/* ; do
-  echo $i
+cd files
+
+for i in * ; do
+  date=`date +%s`
+  if [[ ! -h ~/.$i ]]; then
+    mkdir ~/tmp/dotfiles-$date
+    cp ~/.$i ~/tmp/dotfiles-$date/
+  fi
+
+  rm ~/.$i ; ln -s ~/Code/dotfiles/files/$i ~/.$i
 done
