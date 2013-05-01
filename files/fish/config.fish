3c735a50ec7632e6aa707abcff5d4b89ae239c27
Erik Cunningham <djtrinary@gmail.com>
2013-05-01 14:27:54 -0600

    add fish without oh-my-fish, cleaned up vimrc, tmux status sizes

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -1,4 +1,12 @@
-set BROWSER 'open'
+# Theme
+set fish_theme robbyrussell
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
 
 alias grep="grep --color=auto"
 alias scr='vim ~/util/scratch'
@@ -21,6 +29,11 @@ alias vi=vim
 
 set -gx CDPATH /Users/ewc/Code .
 
+set -gx EDITOR vim
+
+set -gx GOROOT /usr/local/Cellar/go/1.1beta2/
+set -gx GOPATH /usr/local/lib/go
+
 set -gx PATH /usr/X11/bin /Users/ewc/Code/incanter/bin $PATH
 set -gx PATH /bin /usr/bin /sbin /usr/sbin /usr/local/sbin /usr/local/pgsql/bin /usr/local/git/bin $PATH
 set -gx PATH /opt/local/Library/Frameworks/Python.framework/Versions/2.6/bin $PATH
@@ -28,10 +41,31 @@ set -gx PATH /Users/ewc/bin/android-sdk-mac_x86/tools /Developer/usr/bin $PATH
 set -gx PATH /Users/ewc/Code/crawl/crawl-ref/source $PATH
 set -gx PATH /Users/ewc/.rbenv/bin /Users/ewc/.rbenv/shims $PATH
 set -gx PATH /opt/local/bin /Users/ewc/bin /opt/local/lib/postgresql86/bin /usr/local/mysql/bin /usr/local/bin $PATH
-
-. $HOME/.config/fish/functions/rbenv.fish
-
-function fish_prompt -d "Write the prompt"
-  printf '%s@mac%s%s%s> ' (whoami) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+function _git_branch_name
+  echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
 end
 
+function _is_git_dirty
+  echo (git status -s --ignore-submodules=dirty ^/dev/null)
+end
+
+function fish_prompt
+  set -l blue (set_color blue)
+  set -l green (set_color green)
+  set -l normal (set_color normal)
+
+  set -l arrow "λ"
+  set -l cwd $blue(basename (prompt_pwd))
+
+  if [ (_git_branch_name) ]
+    set git_info $green(_git_branch_name)
+    set git_info ":$git_info"
+
+    if [ (_is_git_dirty) ]
+      set -l dirty "*"
+      set git_info "$git_info$dirty"
+    end
+  end
+
+  echo -n -s $cwd $git_info $normal ' ' $arrow ' '
+end
