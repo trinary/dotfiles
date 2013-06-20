889f5104d7c6b1edae5930a04834da2688946c81
Erik Cunningham <djtrinary@gmail.com>
2013-06-20 15:57:41 -0600

    load functions/*, add servethis, get rid of rbenv stuff

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -78,3 +78,5 @@ end
 function cdf
   cd (osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')
 end
+
+. ~/.config/fish/functions/*
---
+++ files/fish/functions/mine.fish
@@ -0,0 +1,4 @@
+function servethis
+  python -m SimpleHTTPServer
+end
+
