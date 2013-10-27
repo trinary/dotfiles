e6d8ac99615450df3a24180d7b505d23e9afff29
Erik Cunningham <djtrinary@gmail.com>
2013-10-27 12:20:06 -0600

    add a create session tmux command

--- files/fish/functions/mine.fish
+++ files/fish/functions/mine.fish
@@ -10,5 +10,10 @@ function -d "Attach to a running tmux session." tmux-attach-session
   tmux attach -t $argv
 end
 
+function -d "Create a named tmux session" tmux-new-session
+  tmux new-session -s $argv
+end
+
 alias tl=tmux-list-sessions
 alias ta=tmux-attach-session
+alias tn=tmux-new-session
